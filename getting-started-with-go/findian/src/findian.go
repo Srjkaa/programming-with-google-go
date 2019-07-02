6ec3f5768050e1e6a826ffda0685d8c77c4786c9
Srjkaa <srjkaa@yandex.by>
2019-07-03 00:09:34 +0300

    Added solution for 'findian' exercise

--- getting-started-with-go/findian/src/findian.go
+++ getting-started-with-go/findian/src/findian.go
@@ -1,1 +1,27 @@
-package src
+package main
+
+import (
+	"fmt"
+	"bufio"
+	"os"
+	"strings"
+)
+
+func main() {
+	var givenString string
+	fmt.Println("Enter a string:")
+	scanner := bufio.NewScanner(os.Stdin)
+	if scanner.Scan() {
+		givenString += scanner.Text()
+	}
+
+	givenStringInLowerCase := strings.ToLower(givenString)
+	isStartsWithRuneI := strings.IndexRune(givenStringInLowerCase, 'i') == 0
+	isEndsWithRuneN := strings.IndexRune(givenStringInLowerCase, 'n') == len(givenString) - 1
+	isContainsRuneA := strings.ContainsRune(givenStringInLowerCase, 'a')
+	if (isStartsWithRuneI && isEndsWithRuneN && isContainsRuneA) {
+		fmt.Println("Found!")
+		return
+	}
+	fmt.Println("Not Found!")
+}
