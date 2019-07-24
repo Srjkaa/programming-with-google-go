499d69b5942477bc9f92631edf15cd46b0574c18
Srjkaa <srjkaa@yandex.by>
2019-07-25 01:36:53 +0300

    physics.go assignement solution

--- functions-methods-and-interfaces-in-go/physics/src/physics.go
+++ functions-methods-and-interfaces-in-go/physics/src/physics.go
@@ -1,5 +1,29 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"math"
+)
 
+func genDisplaceFn(acceleration, initialVelocity, initialDisplacement float64) func (float64) float64 {
+	fn := func(time float64) float64 {
+		return 0.5 * acceleration * math.Pow(time, 2) + initialVelocity * time + initialDisplacement
+	}
+	return fn
+}
+
+func promptUserInput(greeting string) float64 {
+	fmt.Printf("%s: ", greeting)
+	var value float64
+	fmt.Scan(&value)
+	return value
+}
+
+func main() {
+	acceleration := promptUserInput("acceleration")
+	initialVelocity := promptUserInput("initial velocity")
+	initialDisplacement := promptUserInput("initial displacement")
+	time := promptUserInput("time")
+	fn := genDisplaceFn(acceleration, initialVelocity, initialDisplacement)
+	fmt.Printf("Displacement after the entered time: %f\n", fn(time))
 }
