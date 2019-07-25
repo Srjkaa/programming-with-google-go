0aa2f99700e867b18ebff311b22e7969e40aa770
Srjkaa <srjkaa@yandex.by>
2019-07-26 00:31:50 +0300

    Added Animal struct and associated methods

--- functions-methods-and-interfaces-in-go/go-oop/src/go-oop.go
+++ functions-methods-and-interfaces-in-go/go-oop/src/go-oop.go
@@ -1,4 +1,23 @@
 package main
 
+type Animal struct {
+	food string
+	locomotion string
+	noise string
+}
+
+func (animal Animal) Eat() string {
+	return animal.food
+}
+
+func (animal Animal) Move() string {
+	return animal.locomotion
+}
+
+func (animal Animal) Speak() string {
+	return animal.noise
+}
+
 func main() {
+
 }
